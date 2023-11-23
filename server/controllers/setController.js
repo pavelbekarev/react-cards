cec139be02abb6f9191d2cc3bddd0f34928cbc6c
pavelbekarev <bekarev.ps@gmail.com>
2023-11-23 17:05:35 +0500

    работа 5

--- server/controllers/setController.js
+++ server/controllers/setController.js
@@ -37,6 +37,65 @@ class SetController {
             return res.status(500).json({message: "Server error"});
         }
     }
+
+    async deleteSet(req, res) {
+        try {
+            const { _id } = req.body;
+
+            const candidateSet = await Set.findOne({_id: req.query.id})
+
+            if (candidateSet) {
+                await candidateSet.deleteOne()
+                return res.status(200).json({message: "Set has been removed"})
+            }
+
+            else {
+                return res.status(400).json({message: `Set with id ${_id} not found`});
+            }
+        }
+
+        catch (e) {
+
+        }
+    }
+
+
+    async updateSet(req, res) {
+        try {
+            const { _id, name, description } = req.body;
+            const alias = cyrillicToTranslit().transform(name, "_");
+
+            const candidateName = await Set.findOne({name: name});
+            const candidateAlias = await Set.findOne({alias: alias});
+
+            const candidateSet = await Set.findOne({_id: req.query.id})
+
+            if (candidateSet) {
+                if (candidateName || candidateAlias) {
+                    return res.status(400).json({message: `Set with name ${name} already exist`});
+                }
+
+                else {
+                    await Set.findByIdAndUpdate(_id, {
+                        name: name,
+                        description: description,
+                        alias: alias,
+                    },
+                    {new: true})
+
+                    return res.json({message: "Set was updated"})
+                }
+            }
+
+            else {
+                return res.status(400).json({message: `Set with id ${_id} not found`});
+            }
+        }
+
+        catch (e) {
+
+        }
+    }
 }
 
 
--- server/routes/set.routes.js
+++ server/routes/set.routes.js
@@ -5,5 +5,8 @@ const router = new Router();
 
 router.post('/create', setController.createSet);
 router.get('/getSets', setController.getSets);
+router.delete('/delete', setController.deleteSet);
+router.post("/update", setController.updateSet)
+
 
 module.exports = router;
